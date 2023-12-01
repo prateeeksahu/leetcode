f4cf56cfd60b943881b6c9640e64d4e2fbfb6052
prateeeksahu <100373713+prateeeksahu@users.noreply.github.com>
2023-12-02 00:29:23 +0530

    011223

---
+++ bestTimeToBuyAndSellStock.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solutions/
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = 0;
+        int max = 0;
+        int mini = -1;
+        int maxi = 0;
+
+        for(int i = 0;i<prices.length;i++){
+            int p = prices[i];
+
+            if(x>min){
+
+            }
+        }
+    }
+}
