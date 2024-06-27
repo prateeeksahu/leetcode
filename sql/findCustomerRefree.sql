ee39889ee36aa7db7d673cded02ff456f221b2df
prateeeksahu <prateekshahu9422@gmail.com>
2024-06-27 23:25:22 +0530

    2706224

--- sql/findCustomerRefree.sql
+++
@@ -1,4 +0,0 @@
--- https://leetcode.com/problems/find-customer-referee/?envType=study-plan-v2&envId=top-sql-50
-
-# Write your MySQL query statement below
-SELECT name FROM Customer WHERE referee_id <> 2 or referee_id is null;
--- sql/invalidTweets.sql
+++
@@ -1,5 +0,0 @@
--- https://leetcode.com/problems/invalid-tweets/?envType=study-plan-v2&envId=top-sql-50
-
-# Write your MySQL query statement below
-
-select tweet_id from tweets where char_length(content) > 15;
